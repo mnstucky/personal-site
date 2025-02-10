a13820f729901fef7b8abf2abe5f5c7b55f067ab
Matt Stucky <59749326+mnstucky@users.noreply.github.com>
2025-02-10 19:43:58 +0000

    Fix terminal size

--- app/page.tsx
+++ app/page.tsx
@@ -363,7 +363,7 @@ export default function Home() {
           transition-all
           duration-300
           flex-1
-          overflow-y-a  
+          overflow-y-auto  
           h-full`}
       >
         <div className='h-50 bg-zinc-700 rounded-t-md shadow-md px-4 py-2 flex items-center justify-between'>
@@ -389,7 +389,7 @@ export default function Home() {
           rounded-b-md 
           p-3 
           relative 
-          overflow-y-a  
+          overflow-y-auto
           overflow-x-hidden
           [&::-webkit-scrollbar]:w-2
           [&::-webkit-scrollbar-track]:bg-zinc-100
@@ -398,6 +398,7 @@ export default function Home() {
           dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500
           flex-1
           max-h-[calc(100%-2.5rem)]
+          min-h-[calc(100%-2.5rem)]
           terminal
           `}
         >
